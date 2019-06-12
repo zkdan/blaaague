fd106f1e75c5dc58716c3fb2073d1701ded0b020
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2019-06-12 16:54:18 -0400

    conflicts

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 'use strict';
 
 module.exports = {
-<<<<<<< HEAD
   url: 'https://www.zoecodes.com',
   title: 'Blog by Zoe Daniels',
   subtitle: 'Conversational, easy-to-understand explanations of web development topics',
-=======
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
->>>>>>> 9d14ba355c15a3887b654ad96b24b4b58eb2171a
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -19,7 +13,6 @@ module.exports = {
       label: 'Articles',
       path: '/'
     },
-<<<<<<< HEAD
     // {
     //   label: 'About me',
     //   path: '/pages/about'
@@ -39,28 +32,6 @@ module.exports = {
       github: 'zkdan',
       // rss: '#',
       // vkontakte: '#'
-=======
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
-  ],
-  author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
->>>>>>> 9d14ba355c15a3887b654ad96b24b4b58eb2171a
     }
   }
 };
